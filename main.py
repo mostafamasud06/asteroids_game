cdb7797332758feca0b4d7540e45d1d8a0d71aa0
mostafamasud06 <mostafamasud312@gmail.com>
2026-01-17 16:20:27 +0600

    Asteroid added first phase(No Collision

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock=pygame.time.Clock()
+    updatable=pygame.sprite.Group()
+    drawable =pygame.sprite.Group()
+
+    asteroids=pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers=(updatable)
+
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     dt=0
+
     while True:
         log_state()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+    
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt= clock.tick(60)/1000
 if __name__ == "__main__":
